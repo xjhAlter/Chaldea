31bc0262754ce1cfae377f8bee408af9440a8dd6
xjh <1367270948@qq.com>
2019-08-19 15:16:59 +0800

    首页展示优化

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="home">
-    这里是首页啦
+    <!-- 排序不符合通常的逻辑，无需排序的场景可以考虑使用 -->
     <div class="random-box">
-      <div v-for="n in Math.floor(Math.random()*100)" :key="n" class="box color-default" :style="{'width':getSize(),'height':getSize()}">{{n}}</div>
+      <div v-for="n in Math.floor(Math.random() * 100 + 1)" :key="n" class="box color-primary" :style="{'width':'380px','height':getSize()}">{{n}}</div>
     </div>
   </div>
 </template>
@@ -15,13 +15,16 @@ export default {
 
     }
   },
+  created () {
+
+  },
   methods: {
 
   },
   computed: {
     getSize () {
       return function () {
-        return Math.floor(Math.random() * 400 + 20) + 'px'
+        return Math.floor(Math.random() * 400 + 100) + 'px'
       }
     }
   }
@@ -34,19 +37,23 @@ export default {
     height: auto;
     background-color: #fff;
     .random-box {
-      display: flex;
-      flex-wrap: wrap;
-      /*column-count: 3;*/
-      /*column-gap: 0;*/
+      padding: 20px;
+      -moz-column-count:3; /* Firefox */
+      -webkit-column-count:3; /* Safari 和 Chrome */
+      column-count:3;
+      -moz-column-gap: 10px; /* Firefox */
+      -webkit-column-gap: 10px; /* Safari 和 Chrome */
+      column-gap: 10px;
+
       .box {
         display: flex;
         justify-content: center;
         align-items: center;
         font-size: 20px;
         font-weight: bold;
-        background-color: #e8e8e8;
-        margin: 5px;
-        /*break-inside: avoid;*/
+        background-color: #eee;
+        margin-bottom: 10px;
+        break-inside: avoid;  //控制文本块分解成单独的列，以免列表的内容跨列
       }
     }
   }
